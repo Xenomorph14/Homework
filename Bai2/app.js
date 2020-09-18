ea47742ff0e65a982c54dda70b7b01876684f101
Phạm Hữu Thắng <62198279+Xenomorph14@users.noreply.github.com>
2020-09-18 21:36:11 +0700

    up

--- Bai2/app.js
+++ Bai2/app.js
@@ -2,20 +2,28 @@
 let userInput=prompt("Moi nhap");
 let arr=userInput.split("");
 arrNew=[];
-let checkKey;
+let bool;
 let i = 0;
 while (i < arr.length - 1) {
-    checkKey = true;
+    bool = true;
     let j = i + 1;
-    while (j < arr.length) {
+    while (j < arr.length) 
+    {
         if (arr[i] == arr[j])
         {
             arr.splice(j, 1);
-            checkKey = false;
-        } else { j++ }
+            bool = false;
+        } else 
+            { 
+                j++; 
+            }
     }
-    if (checkKey == false) {
-        arr.splice(i, 1);
-    } else { i++ }
+    if (bool == false) 
+        {
+            arr.splice(i, 1);
+        } else 
+            {
+                i++ ;
+            }
 }
 console.log(arr);
